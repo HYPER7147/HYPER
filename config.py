c0298116861f88506ea0360096b278009cfb23fa
HYPER7147 <op0395687@gmail.com>
2024-11-12 09:55:32 +0530

    HYPER

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SEAZONE_SUPPORT_BOT)
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SEAZONE_SUPPORT_BOT")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SEAZONE_SUPPORT_UPDATE_BOT")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
